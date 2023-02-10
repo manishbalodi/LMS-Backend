a81adc56d7fb766b34a9b2bd48a4f8024ef540cd
Balodi <2249662@cognizant.com>
2023-02-10 17:12:12 +0530

    junit added

--- courseservice/src/test/java/com/lms/courseservice/service/CourseServiceImplTest.java
+++ courseservice/src/test/java/com/lms/courseservice/service/CourseServiceImplTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -48,4 +50,39 @@ public class CourseServiceImplTest {
 		Optional<CourseDto> c = courseServiceImpl.getCourseById(1);
 		assertNotNull(c);
 	}
+	
+	@Test
+	public void addCourseTest(){
+		Course course = new Course(1,"manish course",5,"ydfughjg","vhjhjj","hjgjhgj","bbbb");
+		when(courseRepository.save(Mockito.any())).thenReturn(course);
+		CourseDto courseDto = new CourseDto(1,"manish course",5,"ydfughjg","vhjhjj","hjgjhgj","bbbb");
+		Optional<CourseDto> c = courseServiceImpl.addCourse(courseDto);
+		assertNotNull(c);
+	}
+	
+	@Test
+	public void deleteCourseTest(){
+//		Course course = new Course(1,"manish course",5,"ydfughjg","vhjhjj","hjgjhgj","bbbb");
+//		Mockito.when(courseRepository.deleteById(Mockito.anyLong()));
+//		when(courseRepository.deleteById(Mockito.anyLong())).thenReturn();
+//		courseRepository.deleteById((long) 1);
+		courseServiceImpl.deleteCourse(1);
+//		verify(courseRepository).deleteById(1);
+		Mockito.verify(courseRepository).deleteById((long) 1);
+//		CourseDto courseDto = new CourseDto(1,"manish course",5,"ydfughjg","vhjhjj","hjgjhgj","bbbb");
+//		Optional<CourseDto> c = courseServiceImpl.addCourse(courseDto);
+//		assertNotNull(c);
+	}
+	
+	@Test
+	public void getAllCourses(){
+		List<Course> courseList = new ArrayList<>();
+		courseList.add(new Course(1,"manish course",5,"ydfughjg","vhjhjj","hjgjhgj","bbbb"));
+		courseList.add(new Course(2,"manish course",5,"ydfughjg","vhjhjj","hjgjhgj","bbbb"));
+		Mockito.when(courseRepository.findAll()).thenReturn(courseList);
+		Optional<List<CourseDto>> dtoList = courseServiceImpl.getAllCourses();
+		assertEquals(2, dtoList.get().size());
+		
+	}
+	
 }
